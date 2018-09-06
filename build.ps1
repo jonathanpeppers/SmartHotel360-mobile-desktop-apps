52acdea4a6eb90c9085e0276254053b8c3c48297
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-09-06 10:28:24 -0500

    Beefed up the script
    Run /t:Build, /t:SignAndroidPackage, /t:Install
    Print summary at the end

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
 $sln = '.\src\SmartHotel.Clients.sln'
 $csproj = '.\src\SmartHotel.Clients\SmartHotel.Clients.Android\SmartHotel.Clients.Android.csproj'
 $xaml = '.\src\SmartHotel.Clients\SmartHotel.Clients\App.xaml'
-$verbosity = 'minimal'
+$adb = 'C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe'
+$packageName = 'com.microsoft.smarthotel'
+$verbosity = 'quiet'
 
 $nuget = '.\nuget.exe'
 if (!(Test-Path $nuget)) {
@@ -19,42 +21,53 @@ function Touch {
 }
 
 function MSBuild {
+    param ([string] $msbuild, [string] $target, [string] $binlog)
+
+    & $msbuild $csproj /t:$target /v:$verbosity /bl:$binlog
+    if (!$?) {
+        exit
+    }
+
+    # So git clean call doesn't delete
+    & git add $binlog
+}
+
+function Profile {
     param ([string] $msbuild, [string] $version)
     
-    # Reset working copy
+    # Reset working copy & device
+    & $adb uninstall $packageName
     & git clean -dxf
     & $nuget restore $sln
 
-    # First build
-    $binlog = "./first-$version.binlog"
-    & $msbuild $csproj /v:$verbosity /bl:$binlog
-    if (!$?) {
-        exit
-    }
-    & git add $binlog
+    # First
+    MSBuild -msbuild $msbuild -target 'Build' -binlog "./first-build-$version.binlog"
+    MSBuild -msbuild $msbuild -target 'SignAndroidPackage' -binlog "./first-package-$version.binlog"
+    MSBuild -msbuild $msbuild -target 'Install' -binlog "./first-install-$version.binlog"
 
-    # Second build
-    $binlog = "./second-$version.binlog"
-    & $msbuild $csproj /v:$verbosity /bl:$binlog
-    if (!$?) {
-        exit
-    }
-    & git add $binlog
+    # Second
+    MSBuild -msbuild $msbuild -target 'Build' -binlog "./second-build-$version.binlog" 
+    MSBuild -msbuild $msbuild -target 'SignAndroidPackage' -binlog "./second-package-$version.binlog"
+    MSBuild -msbuild $msbuild -target 'Install' -binlog "./second-install-$version.binlog"
 
-    # Third build (Touch XAML)
-    $binlog = "./third-$version.binlog"
+    # Third (Touch XAML)
     Touch $xaml
-    & $msbuild $csproj /v:$verbosity /bl:$binlog
-    if (!$?) {
-        exit
-    }
-    & git add $binlog
+    MSBuild -msbuild $msbuild -target 'Build' -binlog "./third-build-$version.binlog"
+    MSBuild -msbuild $msbuild -target 'SignAndroidPackage' -binlog "./third-package-$version.binlog"
+    MSBuild -msbuild $msbuild -target 'Install' -binlog "./third-install-$version.binlog"
 }
 
 # 15.8.2
 $msbuild = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe'
-MSBuild -msbuild $msbuild -version '15.8'
+Profile -msbuild $msbuild -version '15.8'
 
-# 15.9 P2
-$msbuild = 'C:\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\MSBuild\15.0\Bin\MSBuild.exe'
-MSBuild -msbuild $msbuild -version '15.9'
+# 15.9 P2 (TODO)
+#$msbuild = 'C:\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\MSBuild\15.0\Bin\MSBuild.exe'
+#Profile -msbuild $msbuild -version '15.9'
+
+# Print summary of results
+$logs = Get-ChildItem .\*.binlog
+foreach ($log in $logs) {
+    $time = & $msbuild $log | Select-Object -Last 1
+    Write-Host "$log $time"
+}
